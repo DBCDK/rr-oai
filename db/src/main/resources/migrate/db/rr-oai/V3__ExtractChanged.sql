8b4a37e17c162763d79b43018d5c7c2cfe498fc3
Morten Bøgeskov <mb@dbc.dk>
2019-11-14 06:50:13 +0100

    Rename id's fron agency:recordid to agency-recordid

--- db/src/main/resources/migrate/db/rr-oai/V3__ExtractChanged.sql
+++ db/src/main/resources/migrate/db/rr-oai/V3__ExtractChanged.sql
@@ -1,7 +1,8 @@
-CREATE TABLE t AS  SELECT pid, changed, setspec, gone FROM oairecords JOIN oairecordsets USING(pid);
+CREATE TABLE t AS  SELECT REGEXP_REPLACE(pid, '^([^:]*):', '\1-') AS pid, changed, setspec, gone FROM oairecords JOIN oairecordsets USING(pid);
 
 DROP TABLE oairecordsets;
 ALTER TABLE t RENAME TO oairecordsets;
+UPDATE oairecords SET pid=REGEXP_REPLACE(pid, '^([^:]*):', '\1-');
 
 ALTER TABLE oairecordsets ADD CONSTRAINT oairecordsets_pk PRIMARY KEY (pid, setSpec);
 ALTER TABLE oairecordsets ADD CONSTRAINT oairecordsets_pid_fk FOREIGN KEY (pid) REFERENCES oairecords (pid);
