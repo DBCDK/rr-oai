9bebb28a0977813242669f1f9314888fcc1a4856
Thomas Pii <thp@dbc.dk>
2021-01-22 14:46:04 +0100

    added BCI set description

---
+++ db/src/main/resources/migrate/db/rr-oai/V5__OAI4BciSet.sql
@@ -0,0 +1,5 @@
+
+-- OAI-4 Oprettelse af BCI-sæt
+
+UPDATE oaisets SET description='Poster fra base 870970 med ejerkode ''700300'' (Flersproglig Samling, Det Kgl. Bibliotek) i felt 996a. Betalingsprodukt - kræver adgangskode.'
+ WHERE setSpec='fdepot';
